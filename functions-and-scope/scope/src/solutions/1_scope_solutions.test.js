6e62be4255ad7af1c1745434fb0671631fe5815f
Evan Cooper <evcooper@wayfair.com>
2019-07-08 09:39:12 -0400

    Clean up solutions

--- functions-and-scope/scope/src/solutions/1_scope_solutions.test.js
+++ functions-and-scope/scope/src/solutions/1_scope_solutions.test.js
@@ -1,19 +1,4 @@
 // Exercise 1.a
-
-var love = "What is love?";
-
-(function() {
-  love = "woah-woah-woah, oh, oh";
-})();
-
-(function() {
-  var love = "Baby don't hurt me";
-})();
-
-// One of the functions above changes the local love, the other changes a global love...
-
-// Assign the correct answers to the two variables below by assigning to the UPPERCASE CONSTATNS, what is the global & local?
-
 // What is love? (the global version)
 const WHAT_IS_GLOBAL_LOVE = "woah-woah-woah, oh, oh";
 
@@ -21,54 +6,13 @@ const WHAT_IS_GLOBAL_LOVE = "woah-woah-woah, oh, oh";
 const WHAT_IS_LOCAL_LOVE = "Baby don't hurt me";
 
 // Exercise 1.b
-// So the difference above is that in one method we defined a variable and in the other we do not.
-// So what is the result of this?
-function noop() {}
-
-// Here we are going to
-(function(log) {
-  log(love);
-
-  var love = "No more";
-})(noop);
-
-// QUESTION - Answer the question by assigning the correct value to UPPER_CASE CONSTANT below
-// What is the logged love? (the hoisted version)
 const WHAT_IS_HOISTED_LOVE = undefined;
 
-// TIP: Replace the noop method with console.log to see the answer if you are stuck.
-
-// What this exercise demonstrates is "hoisting" in JavaScript. In JavaScript every variable declared in a function is "hosited" to the top of the function
-// no matter where in the function it is defined. This
-
-// (OPTIONAL) - rewrite the method to show what is really happening
-(function(log) {
-  var love = undefined;
-  log(love);
-
-  love = "No more";
-})(noop);
-// FIN
-
 // Exercise 1.c
-// ES6 Syntax introduced two new ways to declare variables with let and const keywords. let & const also introduce BLOCK SCOPE
-
-// Take a look at the code below and assign WHAT_IS_BLOCK_LOVE to the value logged
-(function(log) {
-  love = "What is love?";
-  if (true) {
-    let love = "No more";
-  }
-  log(love);
-})(noop);
-
-// QUESTION - Answer the question by assigning the correct value to UPPER_CASE CONSTANT below
-// What is logged love? (the block scope version)
 const WHAT_IS_BLOCK_LOVE = "What is love?";
 
 
 // Exercise 2
-
 var whoStoleTheCookies;
 var whoseFingerprintsAreOnItNow;
 
@@ -165,9 +109,3 @@ function _require(moduleName) {
   return modules[moduleName].exports;
 }
 
-// EXERCISE TODOs
-// 1 - Make sure each file above is a module by creating a scope for each one
-// 2 - Make sure each file/module has access to it's own "module" object
-// 3 - Make sure the result of _require('main') return the number 6!
-
-// The test will call _require('main') and expect it to return the number 6

1f8764197e0720e2f3ff8e7a8ba6b4756835c0c1
Evan Cooper <evcooper@wayfair.com>
2019-07-10 20:29:11 -0400

    Correcting testing paths issue

--- testing/component-testing/src/__tests__/3-user-interaction.test.js
+++ testing/component-testing/src/__tests__/3-user-interaction.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { shallow, mount } from "enzyme";
-import setup from "../../test";
+import setup from "../../test/setup";
 
 `
 ⬇️

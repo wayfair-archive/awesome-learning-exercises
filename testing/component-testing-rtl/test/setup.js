4e5c38b02d455eb294357f0b8bb271b35f6d7425
helloitsjoe <jboyle617@gmail.com>
2019-11-04 20:56:42 -0500

    Revert "add window.expect"
    This reverts commit 96597c20c6476b9d859840c041665124e003e19a.

--- testing/component-testing-rtl/test/setup.js
+++ testing/component-testing-rtl/test/setup.js
@@ -1,5 +1,5 @@
-// Making these no-ops so that the codesandbox browser window doesn't error
-window.test = () => {};
-window.expect = () => {};
-
 import '@testing-library/jest-dom/extend-expect';
+
+if (!window.test) {
+  window.test = () => {};
+}
